b808b8ccee579063a33137d56d9afcec67761ada
Gwonil Joo <gwonil3256@gmail.com>
2021-03-29 18:20:43 +0900

    update

--- web/websocket_server.py
+++ web/websocket_server.py
@@ -6,13 +6,18 @@ import websockets
 
 # 클라이언트 접속이 되면 호출된다.
 async def accept(websocket, path):
-    while True:
-        # 클라이언트로부터 메시지를 대기한다.
-        data = await websocket.recv();
-        print("receive : " + data);
+    try:
+        while True:
+            # 클라이언트로부터 메시지를 대기한다.
+            data = await websocket.recv();
+            print("receive : " + data);
 
-        # 클라인언트로 echo를 붙여서 재 전송한다.
-        await websocket.send("echo : " + data);
+            # 클라인언트로 echo를 붙여서 재 전송한다.
+            await websocket.send("echo : " + data);
+
+    finally:
+        print("end")
+        exit(0)
 
 # 웹 소켓 서버 생성.호스트는 localhost에 port는 9998로 생성한다.
 start_server = websockets.serve(accept, "localhost", 9998);
