0f05993aa637615d6f1275e9e57e021fc97ebd96
Gwonil Joo <gwonil3256@gmail.com>
2021-03-30 14:41:59 +0900

    websocket

--- deep_learning/train.py
+++ deep_learning/train.py
@@ -1,6 +1,10 @@
 from dataloader import *
 from model import *
 
+import asyncio
+import websockets
+import functools
+
 parser = argparse.ArgumentParser(description='cls')
 parser.add_argument('--learning_rate', type=float, default=1e-4)
 parser.add_argument('--num_epochs', type=int, default=10)
@@ -10,6 +14,8 @@ parser.add_argument('--saved_dir', type=str, default="../dataset/gwonil/my_cat_d
 parser.add_argument('--model_path', type=str, default=None)
 parser.add_argument('--random_seed', type=int, default='222')
 args = parser.parse_args()
+
+
 
 def train(model, train_loader, val_loader, criterion, optimizer, args, val_every, device):
     print('Start training..')
@@ -37,6 +43,7 @@ def train(model, train_loader, val_loader, criterion, optimizer, args, val_every
                 print('Save model in', args.saved_dir)
                 best_loss = avrg_loss
                 save_model(model, args.saved_dir)
+
 
 
 def validation(epoch, model, data_loader, criterion, device):
@@ -71,6 +78,52 @@ def save_model(model, saved_dir, file_name='best_model.pt'):
     torch.save(check_point, output_path)
 
 
+# 클라이언트 접속이 되면 호출된다.
+async def accept(websocket, path, model, train_loader, val_loader, criterion, optimizer, args, val_every, device):
+    await websocket.send("sstart");
+    try:
+        await websocket.send("start");
+
+        print('Start training..')
+        best_loss = 9999999
+        for epoch in range(args.num_epochs):
+            await websocket.send("epoch : " + str(epoch));
+            for i, (imgs, labels) in enumerate(train_loader):
+                imgs, labels = imgs.to(device), labels.to(device)
+                outputs = model(imgs) 
+                loss = criterion(outputs, labels)   
+
+                optimizer.zero_grad()          
+                loss.backward()           
+                optimizer.step()           
+
+                _, argmax = torch.max(outputs, 1)
+                accuracy = (labels == argmax).float().mean()
+
+                await websocket.send("echo : " + str(loss.item()));
+                await asyncio.sleep(1)
+
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'.format(
+                    epoch+1, args.num_epochs, i+1, len(train_loader), loss.item(), accuracy.item() * 100))
+
+            if (epoch + 1) % val_every == 0:
+                avrg_loss = validation(epoch + 1, model, val_loader, criterion, device)
+                if avrg_loss < best_loss:
+                    print('Best performance at epoch: {}'.format(epoch + 1))
+                    print('Save model in', args.saved_dir)
+                    best_loss = avrg_loss
+                    save_model(model, args.saved_dir)
+
+        await websocket.send("Done!")
+    
+    except:
+        print("Not working!!")
+
+    finally:
+        print("end")
+        #exit(0)
+
+
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
@@ -98,7 +151,17 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
     val_every = 1
-    train(model, train_loader, val_loader, criterion, optimizer, args, val_every, device)
+    #train(model, train_loader, val_loader, criterion, optimizer, args, val_every, device)
+    
+    # 웹 소켓 서버 생성.호스트는 localhost에 port는 9998로 생성한다.
+
+    bound_handler = functools.partial(accept, model=model, train_loader=train_loader, val_loader=val_loader, 
+                                        criterion=criterion, optimizer=optimizer, args=args, val_every=val_every, device=device)
+    start_server = websockets.serve(bound_handler, "localhost", 9998);
+    # 비동기로 서버를 대기한다.
+    asyncio.get_event_loop().run_until_complete(start_server);
+    asyncio.get_event_loop().run_forever();
+
 
 
 
--- web/websocket_server.py
+++ web/websocket_server.py
@@ -2,26 +2,36 @@ import asyncio
 
 # 웹 소켓 모듈을 선언한다.
 import websockets
+import functools
 
+data = [1,2,3,4,5]
 
 # 클라이언트 접속이 되면 호출된다.
-async def accept(websocket, path):
+async def accept(websocket, path, data):
     try:
-        while True:
+        await websocket.send("start");
+        for i in range(5):
             # 클라이언트로부터 메시지를 대기한다.
-            data = await websocket.recv();
-            print("receive : " + data);
+            #data = await websocket.recv();
+            #print("receive : " + data);
 
             # 클라인언트로 echo를 붙여서 재 전송한다.
-            await websocket.send("echo : " + data);
+            await websocket.send("echo : " + str(data[i]));
 
     finally:
         print("end")
-        exit(0)
+        #exit(0)
 
-# 웹 소켓 서버 생성.호스트는 localhost에 port는 9998로 생성한다.
-start_server = websockets.serve(accept, "localhost", 9998);
 
-# 비동기로 서버를 대기한다.
-asyncio.get_event_loop().run_until_complete(start_server);
-asyncio.get_event_loop().run_forever();
+def main():
+    # 웹 소켓 서버 생성.호스트는 localhost에 port는 9998로 생성한다.
+    bound_handler = functools.partial(accept, data=data)
+    start_server = websockets.serve(bound_handler, "localhost", 9998);
+
+    # 비동기로 서버를 대기한다.
+    asyncio.get_event_loop().run_until_complete(start_server);
+    asyncio.get_event_loop().run_forever();
+
+
+if __name__ == "__main__":
+    main()
